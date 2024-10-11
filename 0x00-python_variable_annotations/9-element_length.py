d2e9b338103e5dead67159e512c818e8136aab37
Sandy Obare <obaresandy@gmail.com>
2024-10-11 14:14:18 +0300

    9-element_length.py update: variable type annotation syntax modification

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,10 @@
 """Type-annotated function element_length that takes a list l of strings as
 argument and returns a list of integers representing the lengths of the
 strings"""
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: list[str]) -> list[int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns a list of integers representing the lengths of the
     strings in a given list"""
-    return [len(x) for x in lst]
+    return [(i, len(i)) for i in lst]
