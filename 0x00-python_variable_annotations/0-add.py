8ca276bf6d484936a4369f870afc77e6edcf7d9d
Sandy Obare <obaresandy@gmail.com>
2024-10-11 11:35:14 +0300

    0-add.py initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function add that takes a float a and a float b as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats
+    """
+    return a + b
