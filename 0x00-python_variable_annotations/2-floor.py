de338ee92e442081cbb9a3318d7920661acf51f7
Sandy Obare <obaresandy@gmail.com>
2024-10-11 12:17:22 +0300

    2-floor.py update: pycodestyle==2.5 format update

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Type-annotated function floor which takes a float n as argument and returns the floor of the float.
+Type-annotated function floor which takes a float n as argument and returns
+the floor of the float.
 """
 import math
 
