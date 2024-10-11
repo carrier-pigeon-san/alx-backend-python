5687704b3c952c184c6e85126a3bcd77a8de651c
Sandy Obare <obaresandy@gmail.com>
2024-10-11 12:09:28 +0300

    3-to_str.py initial commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to_str that returns the string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float
+    """
+    return str(n)
