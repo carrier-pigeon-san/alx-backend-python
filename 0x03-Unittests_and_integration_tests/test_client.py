7408389d1a62455f3274e581eaf1b562a2cc7ef5
Sandy Obare <obaresandy@gmail.com>
2024-11-04 22:24:20 +0300

    test_client.py initial commit

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""A github org client
+"""
+from unittest import TestCase
+from unittest.mock import patch
+from parameterized import parameterized
+from unittest.mock import MagicMock
+from typing import Mapping
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(TestCase):
+    """TestGithubOrgClient class
+    """
+
+    @parameterized.expand([
+        ("google", {"response": "google"}),
+        ("abc", {"response": "abc"}),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, response: Mapping,
+                 mock_get_json: MagicMock) -> None:
+        """Tests GithubOrgClient.org method returns correct value
+        """
+        mock_get_json.return_value = response
+        gh = GithubOrgClient(org_name)
+        self.assertEqual(gh.org, response)
+        self.assertEqual(gh.org, response)
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
