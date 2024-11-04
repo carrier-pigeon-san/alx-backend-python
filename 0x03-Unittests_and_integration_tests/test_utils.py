3b020ab38135828b7d5b0b077c96286ae7bcfcbc
Sandy Obare <obaresandy@gmail.com>
2024-11-04 20:34:39 +0300

    test_utils.py update: pep8 formatting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -63,15 +63,19 @@ class TestMemoize(TestCase):
         """Test memoize function"""
 
         class TestClass:
-            
+            """TestClass class"""
+
             def a_method(self):
+                """return 42"""
                 return 42
-            
+
             @memoize
             def a_property(self):
+                """return self.a_method()"""
                 return self.a_method()
-        
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
             tc = TestClass()
             self.assertEqual(tc.a_property, 42)
             self.assertEqual(tc.a_property, 42)
