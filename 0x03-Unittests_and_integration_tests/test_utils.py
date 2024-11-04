50b233f0a1af219b2f61690f10e5e4bf30e0bdeb
Sandy Obare <obaresandy@gmail.com>
2024-11-04 16:02:25 +0300

    test_utils.py update: TestGetJson class, TestGetJson.test_get_json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Unittests for utils.py"""
 from unittest import TestCase
+from unittest.mock import patch
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any
-access_nested_map = __import__('utils').access_nested_map
+from utils import get_json, access_nested_map
+# access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(TestCase):
@@ -34,3 +36,21 @@ class TestAccessNestedMap(TestCase):
         """Test access_nested_map function with exception"""
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """TestGetJson class"""
+
+    @parameterized.expand([
+        ('http://example.com', {'payload': True}),
+        ('http://holberton.io', {'payload': False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self,
+                      url: str,
+                      payload: Mapping,
+                      mock_get
+                      ) -> None:
+        """Test get_json function"""
+        mock_get.return_value.json.return_value = payload
+        self.assertEqual(get_json(url), payload)
